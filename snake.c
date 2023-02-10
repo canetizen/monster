cb32d4dddac5a48b409dce68309d0875f33a598d
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2023-02-10 16:34:15 +0300

    head improved

--- snake.c
+++ snake.c
@@ -4,56 +4,74 @@
 #include <conio.h>
 #include <stdbool.h>
 
+//playground settings
 #define EDGE 25 // EDGE * EDGE playground
 #define DELAY 50 // in miliseconds
+#define EDGE_TYPE '%'
+#define SNAKE_HEAD '0'
+#define BAIT '*'
+
+//control settings
 #define ESC 27
 #define UP 100
 #define DOWN 97
 #define LEFT 119
 #define RIGHT 115
 
-void create_table(char (*table)[EDGE + 1]);
-bool game(char (*table)[EDGE + 1]);
+typedef struct Body{
+	char symbol = ' ';
+	int x = 0;
+	int y = 0;
+	struct Body* next = NULL;
+} Body;
+
+typedef struct {
+	struct Body* snake_head = NULL;
+} Snake;
+
+void create_table(char (*table)[EDGE + 1], Snake*);
+bool game(char (*table)[EDGE + 1], Snake*);
 void print_table(char (*table)[EDGE + 1], int&);
 void delay(double);
 void generate_apple(char (*table)[EDGE + 1]);
-bool choice(char (*table)[EDGE + 1]);
-void insert(int&, int&, char (*table)[EDGE + 1], int&);
-void move_to_left(int&, int&, char (*table)[EDGE + 1], int&, int&);
-void move_to_right(int&, int&, char (*table)[EDGE + 1], int&, int&);
-void move_to_up(int&, int&, char (*table)[EDGE + 1], int&, int&);
-void move_to_down(int&, int&, char (*table)[EDGE + 1], int&, int&);
-
+bool choice(char (*table)[EDGE + 1], Snake*);
+void shift(int, int, char (*table)[EDGE + 1], Snake*);
+void destructor(Snake*);
+Body* constructor (Snake*, int, int, char);
 
 int main() {
+	Snake* snake;
+	int center = EDGE / 2;
 	char table[EDGE][EDGE + 1];
 	srand(time(NULL));
 	do {
+		snake = (Snake*) malloc(sizeof(Snake));
+		snake->snake_head = constructor(snake, center, center, SNAKE_HEAD);
 		system("clear");
-		create_table(table);
+		create_table(table, snake);
 		int initial = 0;
 		print_table(table, initial);
-	} while(game(table));
-	
+	} while(game(table, snake));
+
+	destructor(snake);
 	return EXIT_SUCCESS;
 } 
 
-void create_table(char (*table)[EDGE + 1]) {
+void create_table(char (*table)[EDGE + 1], Snake* snake) {
 	for (int i = 0; i < EDGE; i++)
 		for (int j = 0; j < EDGE; j++)
 			table[i][j] = ' ';
 	
 	for (int i = 0; i < EDGE; i++) {
-		table[0][i] = '%';
-		table[i][0] = '%';
-		table[EDGE - 1][i] = '%';
-		table[i][EDGE - 1] = '%';
+		table[0][i] = EDGE_TYPE;
+		table[i][0] = EDGE_TYPE;
+		table[EDGE - 1][i] = EDGE_TYPE;
+		table[i][EDGE - 1] = EDGE_TYPE;
 	}
-	table[EDGE / 2][EDGE / 2] = '0';	
+	table[EDGE / 2][EDGE / 2] = snake->snake_head->symbol;	
 }
 
-bool game(char (*table)[EDGE + 1]) {
-	int x = EDGE / 2, y = EDGE / 2;
+bool game(char (*table)[EDGE + 1], Snake* snake) {
 	int result = 0;
 	generate_apple(table);
 	bool flag = true;
@@ -62,12 +80,24 @@ bool game(char (*table)[EDGE + 1]) {
 		int ch = (int) getch();
 		do {
 			switch (ch) {
-				case ESC: exit(0);
+				case ESC: 
+				{
+					destructor(snake);
+					exit(0);
+				}
 				case DOWN:
 				{
 					if (previous_direction != UP) {
-						if (table[x][y - 1] != '%' && previous_direction != UP) {
-							move_to_down(x, y, table, result, previous_direction);
+						if (table[snake->snake_head->x][snake->snake_head->y - 1] != EDGE_TYPE) {
+							if (table[snake->snake_head->x][snake->snake_head->y - 1] == '*') {
+								shift(snake->snake_head->x, snake->snake_head->y - 1, table, snake);
+								generate_apple(table);
+								result++;
+							}
+							else {
+								shift(snake->snake_head->x, snake->snake_head->y - 1, table, snake);
+							}		
+							previous_direction = DOWN;				
 						}
 						else
 							flag = false;
@@ -81,8 +111,16 @@ bool game(char (*table)[EDGE + 1]) {
 				case UP:
 				{
 					if (previous_direction != DOWN) {
-						if (table[x][y + 1] != '%' && previous_direction != DOWN) {
-							move_to_up(x, y, table, result, previous_direction);
+						if (table[snake->snake_head->x][snake->snake_head->y + 1] != EDGE_TYPE) {
+							if (table[snake->snake_head->x][snake->snake_head->y + 1] == '*') {
+								shift(snake->snake_head->x, snake->snake_head->y + 1, table, snake);
+								generate_apple(table);
+								result++;
+							}
+							else {
+								shift(snake->snake_head->x, snake->snake_head->y + 1, table, snake);
+							}
+							previous_direction = UP;	
 						}
 						else
 							flag = false;
@@ -96,8 +134,16 @@ bool game(char (*table)[EDGE + 1]) {
 				case LEFT:
 				{
 					if (previous_direction != RIGHT) {
-						if (table[x - 1][y] != '%' && previous_direction != RIGHT) {
-							move_to_left(x, y, table, result, previous_direction);
+						if (table[snake->snake_head->x - 1][snake->snake_head->y] != EDGE_TYPE) {
+							if (table[snake->snake_head->x - 1][snake->snake_head->y] == '*') {
+								shift(snake->snake_head->x - 1, snake->snake_head->y, table, snake);
+								generate_apple(table);
+								result++;
+							}
+							else {
+								shift(snake->snake_head->x - 1, snake->snake_head->y, table, snake);
+							}
+							previous_direction = LEFT;					
 						}
 						else
 							flag = false;
@@ -111,8 +157,16 @@ bool game(char (*table)[EDGE + 1]) {
 				case RIGHT:
 				{
 					if (previous_direction != LEFT) {
-						if (table[x + 1][y] != '%') {
-							move_to_right(x, y, table, result, previous_direction);
+						if (table[snake->snake_head->x + 1][snake->snake_head->y] != EDGE_TYPE) {
+							if (table[snake->snake_head->x + 1][snake->snake_head->y] == '*') {
+								shift(snake->snake_head->x + 1, snake->snake_head->y, table, snake);
+								generate_apple(table);
+								result++;
+							}
+							else {
+								shift(snake->snake_head->x + 1, snake->snake_head->y, table, snake);
+							}
+							previous_direction = RIGHT;
 						}
 						else
 							flag = false;
@@ -130,15 +184,14 @@ bool game(char (*table)[EDGE + 1]) {
 				}
 			}
 			print_table(table, result);
-			delay(DELAY - result * 0.01);
+			delay(DELAY - result * 0.1);
 		} while(!kbhit() && flag);
 	}
-	return choice(table);
+	return choice(table, snake);
 }
 
 void print_table(char (*table)[EDGE + 1], int &result) {
 	system("clear");
-	fflush(stdin);
 	printf("Canetizen Proudly Presents...\n");
 	printf("Press [w] - [a] - [s] - [d] to play. Press [ESC] to quit.\n");
 	printf("\n");
@@ -161,11 +214,11 @@ void generate_apple(char (*table)[EDGE + 1]) {
 	do {
 		x_axis = (rand() %(EDGE - 2)) + 1;
 		y_axis = (rand() %(EDGE - 2)) + 1;
-	} while (table[x_axis][y_axis] == 0);
-	table[x_axis][y_axis] = '*';
+	} while (table[x_axis][y_axis] == SNAKE_HEAD || table[x_axis][y_axis] == '.');
+	table[x_axis][y_axis] = BAIT;
 }
 
-bool choice(char (*table)[EDGE + 1]) {
+bool choice(char (*table)[EDGE + 1], Snake* snake) {
 	char flag;
 	do {
 		printf("You lost. Try again -> [y], Quit -> [n]\n");
@@ -173,45 +226,37 @@ bool choice(char (*table)[EDGE + 1]) {
 	} while (flag != 'y' && flag != 'n');
 	if (flag == 'n'|| (int) flag == 27)
 		return false;
+	destructor(snake);
 	return true;
 }
 
-void insert(int &x, int &y, char (*table)[EDGE + 1], int &result) {
-	if (table[x][y] == '*') {
-		result++;
-		generate_apple(table);
+void destructor(Snake* snake) {
+	Body* temp = snake->snake_head;
+	while (temp != NULL) {
+		Body* temp2 = temp->next;
+		free(temp);
+		temp = temp2;
 	}
-	table[x][y] = '0';
 }
 
-void move_to_left(int &x, int &y, char (*table)[EDGE + 1], int &result, int &previous_direction) {
-	int new_value = x - 1;
-	insert(new_value, y, table, result);
-	table[x][y] = ' ';
-	x--;
-	previous_direction = LEFT;
+Body* constructor(Snake* snake, int x, int y, char symbol) {
+	Body* new_body = (Body*) malloc(sizeof(Body));
+	new_body->symbol = symbol;
+	new_body->x = x;
+	new_body->y = y;
+	return new_body;
 }
 
-void move_to_right(int &x, int &y, char (*table)[EDGE + 1], int &result, int &previous_direction) {
-	int new_value = x + 1;
-	insert(new_value, y, table, result);
-	table[x][y] = ' ';
-	x++;
-	previous_direction = RIGHT;
-}
-
-void move_to_up(int &x, int &y, char (*table)[EDGE + 1], int &result, int &previous_direction) {
-	int new_value = y + 1;
-	insert(x, new_value, table, result);
-	table[x][y] = ' ';
-	y++;
-	previous_direction = UP;
-}
-
-void move_to_down(int &x, int &y, char (*table)[EDGE + 1], int &result, int &previous_direction) {
-	int new_value = y - 1;
-	insert(x, new_value, table, result);
-	table[x][y] = ' ';
-	y--;
-	previous_direction = DOWN;
+void shift(int x, int y, char (*table)[EDGE + 1], Snake* snake) {
+	Body* temp = snake->snake_head;
+	while(temp->next != NULL) {
+		temp->next->x = temp->x;
+		temp->next->y = temp->y;
+		table[temp->next->x][temp->next->y] = temp->next->symbol;
+		temp = temp->next;
+	}
+	table[temp->x][temp->y] = ' ';
+	snake->snake_head->x = x;
+	snake->snake_head->y = y;
+	table[snake->snake_head->x][snake->snake_head->y] = snake->snake_head->symbol;
 }
